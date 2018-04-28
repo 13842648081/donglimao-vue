ba1a0ced8cda0b94175a0f5c40bf654f81b36c8f
li-jingxin <li_jingxin@nhn-st.com>
2018-04-28 17:25:05 +0800

    axios

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,7 @@
-import HelloWorld from '@/components/HelloWorld'
 import Home from '@/components/Home'
-
 export default [
   {
     path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld
-  },
-  {
-    path: '/home',
     name: 'Home',
     component: Home
   }
